f642d81cdcb436f4169c7c58f987bfa3bfeb6dca
Doniapay <sattikgamer409@gmail.com>
2026-07-03 18:18:54 +0600

    Insert DoniaPay payment gateway into database

---
+++ insert_database.php
@@ -0,0 +1,2 @@
+INSERT INTO `payment_gateways` (`id`, `identifier`, `currency`, `title`, `description`, `keys`, `model_name`, `enabled_test_mode`, `status`, `is_addon`, `created_at`, `updated_at`) VALUES
+(21, 'doniapay', 'BDT', 'DoniaPay', 'Pay securely using DoniaPay (bKash, Nagad, Rocket, Cards, etc.)', '{\"api_key\":\"YOUR_API_KEY\"}', 'Payment_model', 1, 1, 0, '1719997440', '1719997440');
